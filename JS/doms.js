18c3d7bc1ae017256c9049d1c597ae6418b5f379
zhaojie <3382865253@qq.com>
2025-09-15 12:06:02 +0800

    fix：抽离dom

---
+++ JS/doms.js
@@ -0,0 +1,15 @@
+//获取dom
+export const doms = {
+  ul: document.querySelector('ul'),
+  audio: document.querySelector('audio'),
+  container: document.querySelector('.container'),
+  songbody: document.querySelector('.songBody'),
+  songName: document.querySelector('.songName'),
+  songBtn: document.querySelector('.songBtn'),
+  nextBtn: document.querySelector('.nextBtn'),
+  lastBtn: document.querySelector('.lastBtn'),
+  endTime: document.querySelector('.endTime'),
+  currentTime: document.querySelector('.currentTime'),
+  rate: document.querySelector('.rate'),
+  progress: document.querySelector('.progress'),
+}
